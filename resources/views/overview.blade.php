2c29a57f57b35a9695792454faae5a700673284f
Bouke <bouke_de_vries@hotmail.com>
2025-07-24 15:06:40 +0200

    updated language to en instead of nl from discord form

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -4,18 +4,18 @@
     <title>Discord</title>
 </head>
 <body>
-    <h1>Stuur bericht naar kanaal</h1>
+    <h1>Send message to channel</h1>
 
     <form method="POST" action="{{ route('discord.handle.submit') }}">
         @csrf
 
-        <label for="message">Bericht:</label><br>
+        <label for="message">Message:</label><br>
         <textarea name="message" id="message">{{ old('message') }}</textarea>
         @error('message')
             <p style="color:red;">{{ $message }}</p>
         @enderror
 
-        <button type="submit">Verzenden</button>
+        <button type="submit">Send</button>
     </form>
 </body>
 </html>
